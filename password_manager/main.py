e54bd023e4b88620ac8311b334b85d4e90a71d44
keple <keplerde@gmail.com>
2023-05-18 21:52:30 +0200

    saving data to->File

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,6 +1,18 @@
 from tkinter import *
 
-# Create a new Tkinter window
+
+# Save password
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open('data.txt', 'a') as file:
+        file.write(f"{website}, {email}, {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
@@ -22,11 +34,19 @@ password_label.grid(row=3, column=0)
 
 #Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1)
-password_entry = Entry(width=20)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "test@gmail.com")
+password_entry = Entry(width=17)
 password_entry.grid(row=3, column=1)
+
+#Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=30, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 
