eb64e6a848ea57a53480afe53e77604d27e1d72d
keple <keplerde@gmail.com>
2023-05-24 14:10:58 +0200

    create a new data.json file if it does not exists

--- password_manager/main.py
+++ password_manager/main.py
@@ -39,16 +39,20 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Ooops", message="Пожалуйста заполните поле!")
     else:
-        # with open("data.json", "r") as data_file:
-        #     #Reading old data
-        #     data = json.load(data_file)
-        #     #Updating data
-        #     data.update(new_data)
-
-        with open("data.json", "w") as data_file:
-            #Saving updated data
-            json.dump(new_data, data_file, indent=4)
-
+        try:
+            with open("data.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(new_data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
