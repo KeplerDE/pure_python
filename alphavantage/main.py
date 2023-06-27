bffbb246f735555e671b8b08ce1641e1c33892b1
keple <keplerde@gmail.com>
2023-06-27 19:01:43 +0200

    to get the news and send the sms

--- alphavantage/main.py
+++ alphavantage/main.py
@@ -1,5 +1,8 @@
 import requests
+from twilio.rest import Client
 
+VIRTUAL_TWILIO_NUMBER = "your virtual twilio number"
+VERIFIED_NUMBER = "your own phone number verified with Twilio"
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -25,3 +28,50 @@ print(yesterday_closing_price)
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
+
+
+#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+
+#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+print(diff_percent)
+
+
+    ## STEP 2: Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+#Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+#If difference percentage is greater than 5 then print("Get News").
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    #Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    three_articles = articles[:3]
+    print(three_articles)
+
+
+    #Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    print(formatted_articles)
+    #Send each article as a separate message via Twilio.
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    #TODO 8. - Send each article as a separate message via Twilio.
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
