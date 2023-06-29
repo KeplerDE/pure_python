f633e695143984c5d235fe16635d538b6a73b67e
keple <keplerde@gmail.com>
2023-06-29 07:28:45 +0200

    http put, delete response

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime
+
 
 USERNAME = "YOUR USERNAME"
 TOKEN = "YOUR SELF GENERATED TOKEN"
@@ -33,3 +35,35 @@ headers = {
 
 response = requests.post(url=graph_endpoint, json=graph_config)
 print(response.text)
+
+# to determine pixel_creation
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+# print(today.strftime("%Y%m%d"))
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometers did you cycle today? "),
+}
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+## PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+## DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
